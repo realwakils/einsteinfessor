8e7aa973008d0560f82883776083fd0bd3620ac3
wakils <villads111@gmail.com>
2021-03-22 20:55:42 +0100

    i hate my life

--- package/routes.py
+++ package/routes.py
@@ -8,7 +8,7 @@ import datetime, package.admin, requests, json, re
 @app.before_request
 def handleUser():
     user_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    print(user_ip)
+    print(flask.request.remote_addr)
     if not User.query.filter_by(ip=user_ip).first():
         user = User(ip=user_ip)
         db.session.add(user)
